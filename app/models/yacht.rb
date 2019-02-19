450307c566d9d18a7a9bfae97e4d677775ace1a1
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2019-02-19 18:52:15 +0100

    Yachts have a photo

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -4,5 +4,11 @@ class Yacht < ApplicationRecord
   # validates :name, presence: true, uniqueness: true
   # validates :price, presence: true, numericality: { only_integer: true }
   # validates :passengers_capacity, presence: true, numericality: { only_integer: true }
-  # mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
+
+  def add_photo(id)
+    url = "http://res.cloudinary.com/duasacvax/image/upload/v1550593316/#{id}.jpg"
+    self.remote_photo_url = url
+    self.save
+  end
 end
