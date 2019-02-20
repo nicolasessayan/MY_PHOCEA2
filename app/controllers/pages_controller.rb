5c9f5bca1cd7e4b9e2f82b6335cef0397009b3dd
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2019-02-20 15:23:39 +0100

    Done buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @yachts = Yacht.all
+    if user_signed_in?
+      @othersyachts = Yacht.where.not(user_id: current_user.id)
+    end
   end
 end
