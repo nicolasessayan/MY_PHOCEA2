1c3fbcde0f260b00eca0c13215618a47b60e1802
Florent Horta <florenthorta@gmail.com>
2019-02-21 18:39:29 +0100

    correct and order the search banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,69 +2,120 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if params[:query_name].present? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].present? && current_user.present?
+    if params[:query_name].present? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("name @@ ?", "%#{params[:query_name]}%")
                      .where("yachts.price <= ?", params[:query_price])
                      .where("yachts.passengers_capacity >= ?", params[:query_guests])
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
                      .where("yachts.price <= ?", params[:query_price])
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.price ASC')
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
                      .where("yachts.passengers_capacity >= ?", params[:query_guests])
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
                      .where("yachts.passengers_capacity >= ?", params[:query_guests])
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.passengers_capacity >= ?", params[:query_guests])
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id)
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
                      .where("ports.address @@ ?", "%#{params[:query_port]}%")
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].present? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.price ASC')
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("ports.address @@ ?", "%#{params[:query_port]}%")
-                                  .select("yachts.user_id != ?", current_user.id)
-
-    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].blank? && current_user.present?
+                                  .where("yachts.user_id != ?", current_user.id).order('yachts.price ASC')
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
                      .where("yachts.price <= ?", params[:query_price])
                      .where("yachts.passengers_capacity >= ?", params[:query_guests])
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].blank? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
                      .where("yachts.price <= ?", params[:query_price])
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].blank? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.price ASC')
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
                      .where("yachts.passengers_capacity >= ?", params[:query_guests])
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].blank? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
                      .where("yachts.passengers_capacity >= ?", params[:query_guests])
-                     .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].blank? && current_user.present?
+                     .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.passengers_capacity >= ?", params[:query_guests])
-                                  .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].blank? && current_user.present?
+                                  .where("yachts.user_id != ?", current_user.id).order('yachts.passengers_capacity ASC')
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
-                                  .select("yachts.user_id != ?", current_user.id)
-    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].blank? && current_user.present?
+                                  .where("yachts.user_id != ?", current_user.id).order('yachts.price ASC')
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].blank? && current_user.nil? != true
       @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
-                                  .select("yachts.user_id != ?", current_user.id)
+                                  .where("yachts.user_id != ?", current_user.id).order('yachts.price ASC')
+
+
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("name @@ ?", "%#{params[:query_name]}%")
+                     .where("yachts.price <= ?", params[:query_price])
+                     .where("yachts.passengers_capacity >= ?", params[:query_guests])
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+                     .where("yachts.price <= ?", params[:query_price])
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+                     .where("yachts.passengers_capacity >= ?", params[:query_guests])
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
+                     .where("yachts.passengers_capacity >= ?", params[:query_guests])
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.passengers_capacity >= ?", params[:query_guests])
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
+                     .where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].present? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("ports.address @@ ?", "%#{params[:query_port]}%")
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+                     .where("yachts.price <= ?", params[:query_price])
+                     .where("yachts.passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+                     .where("yachts.price <= ?", params[:query_price])
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+                     .where("yachts.passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
+                     .where("yachts.passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.passengers_capacity >= ?", params[:query_guests])
+
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.name @@ ?", "%#{params[:query_name]}%")
+
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank? && params[:query_port].blank? && current_user.nil? == true
+      @yachts = Yacht.joins(:port).where("yachts.price <= ?", params[:query_price])
+
     else
       @yachts = Yacht.all
     end
