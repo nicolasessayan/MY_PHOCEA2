9402e972098f45b1dedb3095b97d53d677cbdb56
Olivier Colombani <olcolombani@gmail.com>
2019-02-21 16:58:50 +0100

    map of port

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,12 @@ class PagesController < ApplicationController
     if user_signed_in?
       @othersyachts = Yacht.all.select{ |obj| obj.user_id != current_user.id }
     end
+    @ports = Port.where.not(latitude: nil, longitude: nil)
+    @markers = @ports.map do |port|
+      {
+        lng: port.longitude,
+        lat: port.latitude
+      }
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_105155) do
+ActiveRecord::Schema.define(version: 2019_02_21_141339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_02_21_105155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
