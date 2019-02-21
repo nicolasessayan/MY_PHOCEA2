561ac9de79364baed696ead7ab5afc49802d9205
Florent Horta <florenthorta@gmail.com>
2019-02-21 15:56:32 +0100

    Add search banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,27 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @yachts = Yacht.all
-    if user_signed_in?
-      @othersyachts = Yacht.all.select{ |obj| obj.user_id != current_user.id }
+    if params[:query_name].present? && params[:query_price].present? && params[:query_guests].present?
+      @yachts = Yacht.where("name @@ ?", "%#{params[:query_name]}%")
+                     .where("price <= ?", params[:query_price])
+                     .where("passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].present? && params[:query_price].present? && params[:query_guests].blank?
+      @yachts = Yacht.where("name @@ ?", "%#{params[:query_name]}%")
+                     .where("price <= ?", params[:query_price])
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].present?
+      @yachts = Yacht.where("name @@ ?", "%#{params[:query_name]}%")
+                     .where(" passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].present?
+      @yachts = Yacht.where("price <= ?", params[:query_price])
+                     .where(" passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].blank? && params[:query_price].blank? && params[:query_guests].present?
+      @yachts = Yacht.where(" passengers_capacity >= ?", params[:query_guests])
+    elsif params[:query_name].present? && params[:query_price].blank? && params[:query_guests].blank?
+      @yachts = Yacht.where("name @@ ?", "%#{params[:query_name]}%")
+    elsif params[:query_name].blank? && params[:query_price].present? && params[:query_guests].blank?
+      @yachts = Yacht.where("price <= ?", params[:query_price])
+    else
+      @yachts = Yacht.all
     end
   end
 end
