e3598ddbd9fd215da935c8ed21850c2be3950c0d
Olivier Colombani <olcolombani@gmail.com>
2019-02-22 13:14:20 +0100

    logo and port show image

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2019_02_21_222554) do
     t.index ["yacht_id"], name: "index_bookings_on_yacht_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "ports", force: :cascade do |t|
     t.string "name"
     t.string "photo"
