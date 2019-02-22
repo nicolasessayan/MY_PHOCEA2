7e6d5ad58098a5fa43a6d383275215585b765f68
GuillaumeLaganier <g.laganier@gmail.com>
2019-02-22 10:46:48 +0100

    fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_02_21_141339) do
 
   # These are extensions that must be enabled in order to support this database
